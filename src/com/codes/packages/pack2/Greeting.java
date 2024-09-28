5b1dddb9b4546b62ef926caf71a95545ce30df94
Rakibul1411 <bsse1411@iit.du.ac.bd>
2024-09-28 15:43:05 +0600

    initial commit

--- src/com/codes/packages/pack2/Greeting.java
+++ src/com/codes/packages/pack2/Greeting.java
@@ -1,7 +1,10 @@
 package com.codes.packages.pack2;
 
+import static com.codes.packages.pack2.Message.message;
+
 public class Greeting {
     public static void main(String[] args) {
         System.out.println("I am in pack2 packages..");
+        message();
     }
 }
---
+++ src/com/codes/packages/pack2/Message.java
@@ -0,0 +1,10 @@
+package com.codes.packages.pack2;
+
+public class Message {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    public static void message() {
+        System.out.println("I am in Message Class.");
+    }
+}
